b3b7e9717b92187f7f78dbfe1d38e7ec59f768ba
AlirezaRohany <50998893+AlirezaRohany@users.noreply.github.com>
2020-10-04 22:34:05 +0330

    Registering a Request Handler

--- webGo.go
+++ webGo.go
@@ -1,8 +1,12 @@
 package main
 
 import "fmt"
-// import "net/http"
+import "net/http"
 
 func main(){
 	fmt.Println("Hello Web By Go!")
+	http.HandleFunc("/myindex",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"You have requested: %s\n", r.URL.Path)
+	})	
+	http.ListenAndServe(":80",nil)
 }
