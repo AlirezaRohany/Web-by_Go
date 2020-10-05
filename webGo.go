35c4690cf358e9c3f5f21294949bf6e3cd00c0d2
AlirezaRohany <50998893+AlirezaRohany@users.noreply.github.com>
2020-10-05 23:51:40 +0330

    Fileserver and Get parameter

--- webGo.go
+++ webGo.go
@@ -7,6 +7,15 @@ func main(){
 	fmt.Println("Hello Web By Go!")
 	http.HandleFunc("/myindex",func(w http.ResponseWriter,r *http.Request){
 		fmt.Fprintf(w,"You have requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Welcome to my website!\n")
+		fmt.Fprintf(w,"Get parameters: %s\n",r.URL.Query().Get("token"))
+
 	})	
+
+	fs:=http.FileServer(http.Dir("static/"))
+	http.Handle("/static/",http.StripPrefix("/static/",fs))
+	
 	http.ListenAndServe(":80",nil)
+
+
 }
